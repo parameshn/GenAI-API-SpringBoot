8438b7c1a9c1fc166e0de85b5969db465a39b8e4
paramesh <parameshdarshan1234@gmail.com>
2025-05-26 01:08:10 +0530

    start point

---
+++ GenAI-API-SpringBoot/src/main/java/com/example/genaiapi/GenAiApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.genaiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAiApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GenAiApiApplication.class, args);
+    }
+}
